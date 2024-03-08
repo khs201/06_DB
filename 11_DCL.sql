cc3db62867358324f1c89921430c1da64066f1f6
khs <hanseog201@naver.com>
2024-03-08 17:49:32 +0900

    쉅종

---
+++ 11_DCL.sql
@@ -0,0 +1,145 @@
+/* 계정(사용자)
+
+* 관리자 계정 : 데이터베이스의 생성과 관리를 담당하는 계정.
+                모든 권한과 책임을 가지는 계정.
+                ex) sys(최고관리자), system(sys에서 권한 몇개 제외된 관리자)
+
+
+* 사용자 계정 : 데이터베이스에 대하여 질의, 갱신, 보고서 작성 등의
+                작업을 수행할 수 있는 계정으로
+                업무에 필요한 최소한의 권한만을 가지는 것을 원칙으로 한다.
+                ex) bdh계정(각자 이니셜 계정), workbook 등
+*/
+
+
+
+
+/* DCL(Data Control Language) : 
+
+ 계정에 DB, DB객체에 대한 접근 권한을 부여하고 회수하는 언어
+ 
+- GRANT : 권한 부여
+- REVOKE : 권한 회수
+
+* 권한의 종류
+
+1) 시스템 권한 : DB접속, 객체 생성 권한
+
+CRETAE SESSION   : 데이터베이스 접속 권한
+CREATE TABLE     : 테이블 생성 권한
+CREATE VIEW      : 뷰 생성 권한
+CREATE SEQUENCE  : 시퀀스 생성 권한
+CREATE PROCEDURE : 함수(프로시져) 생성 권한
+CREATE USER      : 사용자(계정) 생성 권한
+DROP USER        : 사용자(계정) 삭제 권한
+DROP ANY TABLE   : 임의 테이블 삭제 권한
+
+
+2) 객체 권한 : 특정 객체를 조작할 수 있는 권한
+
+  권한 종류                 설정 객체
+    SELECT              TABLE, VIEW, SEQUENCE
+    INSERT              TABLE, VIEW
+    UPDATE              TABLE, VIEW
+    DELETE              TABLE, VIEW
+    ALTER               TABLE, SEQUENCE
+    REFERENCES          TABLE
+    INDEX               TABLE
+    EXECUTE             PROCEDURE
+
+*/
+
+/* 관리자 계정 접속 */
+
+--> 오라클 SQL을 예전 방식처럼 사용하겠다라는 코드
+	--> 계정명 작성 시 원하는대로 작성 가능
+	ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 사용자 계정 생성하기
+CREATE USER TEST_USER IDENTIFIED BY TEST1234;
+-- 접속 권한이 없어서 로그인 불가
+
+
+-- [생성한 사용자 계정에 접속 권한 부여하기]
+GRANT CREATE SESSION TO TEST_USER;
+
+/* TEST_USER 접속 */
+
+-- 테이블 생성
+CREATE TABLE TB_MEMBER(
+MEMBER_NO NUMBER PRIMARY KEY,
+MEMBER_ID VARCHAR2(30) NOT NULL,
+MEMBER_PW VARCHAR2(30) NOT NULL);
+
+-- 테이블 생성 권한 없음
+-- + TABLESPACE(테이블 생성 공간)도 없어서 생성 불가
+
+/* 관리자 계정 접속 */
+GRANT CREATE TABLE TO TEST_USER;
+
+-- TABLESPACE(객체 생성 공간) 할당
+ALTER USER TEST_USER
+DEFAULT TABLESPACE USERS
+QUOTA 10M ON USERS;
+
+/* 다시 TEST 유저 계정 접속 */
+
+-- 테이블 생성
+CREATE TABLE TB_MEMBER(
+MEMBER_NO NUMBER PRIMARY KEY,
+MEMBER_ID VARCHAR2(30) NOT NULL,
+MEMBER_PW VARCHAR2(30) NOT NULL);
+-- 성공
+
+------------------------------------
+
+/* ROLE(역할 == 권한의 묶음, 권한명 단순화) 
+ * 
+ * CONNECT(접속) : DB 접속 권한 (CREATE SESSION)
+ * 
+ * RESOURCE(자원) : DB 기본 객체 8개 생성 권한
+ * */
+
+/* 관리자 계정 접속 */
+
+-- ROLE에 묶여있는 권한 확인하기
+SELECT * FROM ROLE_SYS_PRIVS
+WHERE ROLE = 'CONNECT';
+
+-- TEST_USER 계정에 CONNECT, RESOURCE 롤 부여하기
+GRANT CONNECT, RESOURCE TO TEST_USER;
+
+---------------------------------
+
+-- 객체 권한 테스트 --
+
+/* TEST_USER 계정 접속 */
+
+-- KH계정의 EMPLOYEE 테이블 조회하기
+SELECT *
+FROM KH_KHS.EMPLOYEE;
+--> 접근 권한이 없어서 보이지 않음
+
+
+/* KH 계정 접속 */
+
+-- TEST_USER 계정에 EMPLOYEE 테이블 조회 권한 부여하기
+GRANT SELECT ON EMPLOYEE TO TEST_USER;
+
+/* TEST_USER 계정 접속 */
+SELECT * FROM KH_KHS.EMPLOYEE;
+
+/* KH 계정 접속 */
+
+-- TEST_USER의 EMPLOYEE 테이블 조회 권한 회수하기
+REVOKE SELECT ON EMPLOYEE FROM TEST_USER;
+
+/* TEST_USER 계정 접속 */
+SELECT * FROM KH_KHS.EMPLOYEE;
+
+
+
+
+
+
+
